203d0ccefbe42e50b6d213b5b81e26919ba8c1a2
しまなみさん <mikan.sup.all@gmail.com>
2020-06-02 15:52:44 +0900

    webpack.config setting

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+// window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,16 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component(
+//     "example-component",
+//     require("./components/ExampleComponent.vue").default
+// );
+
+// sass ファイル読み込み
+import "../sass/style.scss";
+
+import Vue from "vue";
+import App from "./App.vue";
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +36,14 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+new Vue({
+    // h(App)の引数にApp.vueコンポーネントのオブジェクトが入ってくる
+    // renderではコンポーネントのオブジェクト（テンプレートやメソッドなど）を突っ込んで描画することができる
+    render: h => h(App)
+}).$mount("#app");
+
+// const app = new Vue({
+//     el: "#app",
+//     components: { App },
+//     template: "<App />"
+// });
