baed4379f7f85ddfe6398d6e81e6cac9d5d96bce
しまなみさん <mikan.sup.all@gmail.com>
2020-06-16 23:19:19 +0900

    Larabel Controller Edit

--- app/Http/Controllers/TwitterFollowController.php
+++ app/Http/Controllers/TwitterFollowController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class TwitterFollowController extends Controller
 {
-    //
+  public function follow($id)
+  {
+    // リクエストを送るURL
+    $request_url = 'https://api.twitter.com/1.1/friendships/create.json';
+    // パラメータ
+    $param = [
+      'user_id' => $id,
+    ];
+    $result = \Twitter::post();
+  }
 }
