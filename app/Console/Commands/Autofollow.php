9fcef797a2d4f08b7469ead95a8be990cd2aa56f
しまなみさん <mikan.sup.all@gmail.com>
2020-06-20 23:17:49 +0900

    AutoFollow edit

--- app/Console/Commands/Autofollow.php
+++ app/Console/Commands/Autofollow.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class GetItemFeed extends Command
+class Autofollow extends Command
 {
     /**
      * The name and signature of the console command.
