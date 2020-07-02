cd28395508f8275ff6c081936186efed43c1f572
しまなみさん <mikan.sup.all@gmail.com>
2020-07-03 07:45:11 +0900

    新しいマイグレーションファイルを作成

---
+++ database/migrations/2020_07_03_073328_create_days_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('coin_id')->unsigned();
+            $table->string('tweet')->nullable();
+            $table->foreign('coin_id')->references('id')->on('coins');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('days');
+    }
+}
