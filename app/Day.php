93754873f8934fd4004f9da91b5081c9b5ca93d3
しまなみさん <mikan.sup.all@gmail.com>
2020-07-03 07:43:28 +0900

    新しいモデルを作成

---
+++ app/Day.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Day extends Model
+{
+    protected $fillable = [
+        'coin_id',
+        'tweet',
+    ];
+
+    protected $casts = [
+        'tweet' => 'integer',
+    ];
+
+    public function coin()
+    {
+        return $this->belongdTo('App\Coin');
+    }
+}
---
+++ app/Hour.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hour extends Model
+{
+    protected $fillable = [
+        'coin_id',
+        'tweet',
+    ];
+
+    protected $casts = [
+        'tweet' => 'integer',
+    ];
+
+    public function coin()
+    {
+        return $this->belongdTo('App\Coin');
+    }
+}
