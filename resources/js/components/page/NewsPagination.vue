6f684592b39bea1edbac7ed7f35f16abac0830bd
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-07-14 22:46:54 +0900

    ページ移動時にトップへスクロールするように実装

--- resources/js/components/page/NewsPagination.vue
+++ resources/js/components/page/NewsPagination.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <section class="c-container c-container__news">
-      <div class="p-news__card" v-for="(news, index) in getnewsItems" :key="index">
-        <p>{{ news.title }}</p>
+      <div
+        class="p-news__card"
+        v-for="(news, index) in getnewsItems"
+        :key="index"
+      >
+        <a class="p-news__link" :href="news.url" target="_blank"
+          ><p>{{ news.title }}</p></a
+        >
         <br />
         <p>投稿日：{{ news.pubDate }}</p>
       </div>
@@ -29,47 +35,51 @@
 </template>
 
 <script>
-import Vue from "vue";
-import Paginate from "vuejs-paginate";
-Vue.component("paginate", Paginate);
+import Vue from 'vue';
+import Paginate from 'vuejs-paginate';
+Vue.component('paginate', Paginate);
 export default {
-    data() {
-        return {
-            newsItems: this.news_data,
-            totalPage: this.total_page,
-            parPage: "",
-            currentPage: 1,
-        };
+  data() {
+    return {
+      newsItems: this.news_data,
+      totalPage: this.total_page,
+      parPage: '',
+      currentPage: 1,
+    };
+  },
+  props: ['news_data', 'total_page'],
+  methods: {
+    clickCallback(pageNum) {
+      this.currentPage = pageNum;
+      this.scrollTop();
     },
-    props: ["news_data", "total_page"],
-    methods: {
-        clickCallback(pageNum) {
-            this.currentPage = pageNum;
-        },
-        paginationNumber() {
-            // 表示するページネーションの数を割り出すために、ニュースの総数を表示させる数で割っている
-            this.parPage = this.totalPage / 10;
-        },
+    paginationNumber() {
+      // 表示するページネーションの数を割り出すために、ニュースの総数を表示させる数で割っている
+      this.parPage = this.totalPage / 10;
     },
-    computed: {
-        getnewsItems() {
-            let current = this.currentPage * this.parPage;
-            let start = current - this.parPage;
-            console.log(
-                "カレントページ：" + current + "スタートページ：" + start
-            );
+    scrollTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'auto',
+      });
+    },
+  },
+  computed: {
+    getnewsItems() {
+      let current = this.currentPage * this.parPage;
+      let start = current - this.parPage;
+      console.log('カレントページ：' + current + 'スタートページ：' + start);
 
-            return this.newsItems.slice(start, current);
-        },
-        getPageCount() {
-            return Math.ceil(this.newsItems.length / this.parPage);
-        },
+      return this.newsItems.slice(start, current);
     },
-    created() {
-        this.paginationNumber();
+    getPageCount() {
+      return Math.ceil(this.newsItems.length / this.parPage);
     },
+  },
+  created() {
+    this.paginationNumber();
+  },
 };
 </script>
 
-<style scope>
-</style>
+<style scope></style>
