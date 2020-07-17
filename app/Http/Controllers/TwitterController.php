6faa873c319bb59e4564637c6d05943a3a0c8fca
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-07-18 00:15:50 +0900

    コントローラーの修正

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -14,7 +14,6 @@ class TwitterController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware(['auth','verified']);
         $this->middleware('auth');
     }
 
