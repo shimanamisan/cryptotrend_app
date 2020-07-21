7f75c53a50b7e96e14f6fd12d2bb26ea0444437e
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-07-22 07:19:16 +0900

    watchプロパティを削除

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
     mode: MODE,
-    watch: true,
     performance: {
         maxEntrypointSize: 500000,
         maxAssetSize: 500000,
