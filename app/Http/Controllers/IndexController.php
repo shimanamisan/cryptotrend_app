d81ef526939f2cc41daa549ece7aebfe03143a5d
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-07-22 22:13:21 +0900

    お問い合わせフォーム関係の記述を削除

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,25 +14,4 @@ class IndexController extends Controller
         return view('home');
     }
 
-    // // お問い合わせページの表示
-    // public function contact()
-    // {
-    //     return view('contact.index');
-    // }
-
-    // // お問い合わせ内容の確認
-    // public function confirm(ContactRequest $request)
-    // {
-    //     // フォームリクエストクラスでバリデーション後、
-    //     // フォームから受け取った全ての値を入力確認ページへ渡す
-    //     $form_value = $request->all();
-
-    //     return view('contact.confirm', ['form_value' => $form_value]);
-    // }
-
-    // // お問い合わせ内容の送信
-    // public function finish(ContactRequest $request)
-    // {
-
-    // }
 }
