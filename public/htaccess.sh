55402f7b7a9899752bc2f0f0c3d3cbb4cb8ff135
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-08-04 14:09:00 +0900

    シェルスクリプトテスト

--- public/htaccess.sh
+++ public/htaccess.sh
@@ -2,8 +2,9 @@
 # ローカル用の記述から、本番環境用（httpsを有効にする）の記述に変更する処理です
 
 # 既存の設定ファイルをコピー
-cp .htaccess .htaccess_old
+# cp .htaccess .htaccess_old
 # コピー後に削除
 rm .htaccess
+rm .htaccess_old
 # 本番環境用に用意したものをリネーム
 cp .htaccess_production .htaccess
