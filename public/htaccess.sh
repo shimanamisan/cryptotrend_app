0906c45d365a769f1bcaac63e78c3b1b8bcb7c83
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-08-04 16:10:50 +0900

    シェルスクリプトテスト及び自動フォロー終了時、自動フォローステータスをOFFにする

--- public/htaccess.sh
+++ public/htaccess.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # ローカル用の記述から、本番環境用（httpsを有効にする）の記述に変更する処理です
 
+echo "###############################################"
+echo "##                   START                   ##"
+echo "###############################################"
+
+# 作業ディレクトリを表示
+pwd
 # 既存の設定ファイルをコピー
 cp .htaccess .htaccess_old
+sleep 2
 # コピー後に削除
 rm .htaccess
+sleep 2
 # 本番環境用に用意したものをリネーム
 cp .htaccess_production .htaccess
+sleep 2
 # コピー後に削除
 rm .htaccess_production
 
-git status
-
-pwd
+echo ""
+echo "###############################################"
+echo "##                    END                    ##"
+echo "###############################################"
