d7af6d7d42fb6117aa073447909ff684fe4788c1
shimanamisan <mikan.sup.all@gmail.com>
2020-08-21 23:35:06 +0900

    PHPのバージョンを上げたので、Cron用のスクリプトファイルのパスを変更

--- start_artisan.sh
+++ start_artisan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ロリポップサーバのcronより実行するためのシェルスクリプトです。
-/usr/local/php7.3/bin/php /home/users/0/penne.jp-shimanamisan/web/crypto-trend2020.shimanamisan.com/artisan schedule:run
+/usr/local/php/7.4/bin/php /home/users/0/penne.jp-shimanamisan/web/crypto-trend2020.shimanamisan.com/artisan schedule:run
