8c10d70c7525a403d2dc32dcaaa38cf48528dfb2
shimanamisan <mikan.sup.all@gmail.com>
2021-03-09 07:14:27 +0900

    [add] トラッキングコードを追加

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,4 +1,12 @@
 @section('head')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138230091-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-138230091-6');
+</script>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
